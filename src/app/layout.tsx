6c8f314f72e927fbbff84266fd5336a91afa030c
Pat Zdziabek <204617287+towarzystwomieszkaniowe@users.noreply.github.com>
2025-04-08 15:15:40 +0200

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,18 +28,18 @@ export default function RootLayout({
   return (
     <html lang="pl">
       <head>
-        {/* Google Tag Manager */}
-        <Script
-          id="gtm-script"
-          strategy="afterInteractive"
-          src="https://www.googletagmanager.com/gtm.js?id=GTM-KDPWS2HX"
-        />
+        {/* Google Tag Manager – klasyczny kod */}
         <Script id="gtm-init" strategy="afterInteractive">
           {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-ZVYFV6XNTE');
+            (function(w,d,s,l,i){
+              w[l]=w[l]||[];
+              w[l].push({'gtm.start': new Date().getTime(), event:'gtm.js'});
+              var f=d.getElementsByTagName(s)[0],
+                  j=d.createElement(s), dl=l!='dataLayer'?'&l='+l:'';
+              j.async=true;
+              j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+              f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-KDPWS2HX');
           `}
         </Script>
       </head>
@@ -47,7 +47,7 @@ export default function RootLayout({
         suppressHydrationWarning
         className={`${inter.variable} antialiased`}
       >
-        {/* Google Tag Manager noscript (dla przeglądarek bez JS) */}
+        {/* Google Tag Manager (noscript) – musi być zaraz po <body> */}
         <noscript>
           <iframe
             src="https://www.googletagmanager.com/ns.html?id=GTM-KDPWS2HX"
