c7230e7e190ac6cf7f18b35b2ef8d05b872a0129
Pat Zdziabek <204617287+towarzystwomieszkaniowe@users.noreply.github.com>
2025-04-08 15:09:30 +0200

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import { GoogleTagManager } from '@next/third-parties/google';
-import { GoogleAnalytics } from '@next/third-parties/google';
+import Script from "next/script";
 import "./globals.css";
 import Navbar from './Shared/Navbar';
 import Footer from './Shared/Footer';
@@ -28,15 +27,36 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pl">
-      {/* GTM musi być tutaj – bezpośrednio jako pierwsze dziecko html */}
-      <GoogleTagManager gtmId="GTM-KDPWS2HX" />
-      <head />
+      <head>
+        {/* Google Tag Manager */}
+        <Script
+          id="gtm-script"
+          strategy="afterInteractive"
+          src="https://www.googletagmanager.com/gtm.js?id=GTM-KDPWS2HX"
+        />
+        <Script id="gtm-init" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-ZVYFV6XNTE');
+          `}
+        </Script>
+      </head>
       <body
         suppressHydrationWarning
         className={`${inter.variable} antialiased`}
       >
-        {/* Google Analytics można zostawić w body */}
-        <GoogleAnalytics gaId="G-ZVYFV6XNTE" />
+        {/* Google Tag Manager noscript (dla przeglądarek bez JS) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-KDPWS2HX"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+
         <Navbar />
         {children}
         <ContactForm />
